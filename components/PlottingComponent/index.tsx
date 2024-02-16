4d96da19762501d15bbefad32f70346efa997457
Amal Jose <amaljoseami@gmail.com>
2024-02-16 13:04:57 +0530

    Remove log

--- components/PlottingComponent/index.tsx
+++ components/PlottingComponent/index.tsx
@@ -37,8 +37,6 @@ export default function PlottingComponent() {
     }
   };
 
-  console.log({ image });
-
   const handleObjectAdd: React.MouseEventHandler<HTMLImageElement> = (
     event
   ) => {
